cdc78a4eeba73138a68760b626236275696faea9
lhmet <lhmet@ufsm.br>
2021-11-24 16:42:24 -0300

    update geral

--- R/dados-brutos.R
+++ R/dados-brutos.R
@@ -1,3 +1,82 @@
+pcks <- c("terra", "tidyverse", "here", "checkmate", "metR", "fs", "glue")
+easypackages::libraries(pcks)
+
+## Funções para processamento dos dados netcdf------------------------------
+source(here("R", "data-proc-nc.R"))
+
+## listar todos os arquivos das previsões retrospectivas da precipitação CanCM4i
+nc_dir <- here("output", "prec")
+nc_files <- fs::dir_ls(path = nc_dir, glob = "*.nc")
+
+model_counts <- nc_files_by_model_year(nc_files)
+# modelo       start   end  freq check_span
+# <chr>        <int> <int> <int>      <dbl>
+# 1 CanCM4i       1981  2018    38         38
+# 2 CanSIPSv2     1981  2018    38         38
+# 3 CMC1-CanCM3   1981  2010    30         30
+# 4 CMC2-CanCM4   1981  2010    30         30
+# 5 GEM-NEMO      1981  2018    38         38
+# 6 NASA-GEOSS2S  1981  2017    37         37
+# 7 NCAR-CESM1    1980  2010    31         31
+# 8 NCEP-CFSv2    1982  2010    29         29
+
+
+# como o nome dos arquivos especifica o nome dos modelos
+#model_nm <- "CanCM4i"
+(model_nms <- model_counts$modelo)
+#files_model <- nc_files[grep(model_nms, nc_files)]
+
+# looping para processar dados (excluindo os modelos já processados)
+map(model_nms,
+    function(imodel){
+      # imodel = model_nms[1]
+      cat(imodel, "-------\n", "\n")
+      gc()
+      proc_ncs_by_lt(model = imodel, ncfiles_d = nc_dir)
+    })
+
+
+#----------------------------------------------------------------------------
+nc_dir <- here("output", "prec")
+
+
+## listar todos os arquivos das previsões retrospectivas da precipitação CanCM4i
+nc_files <- fs::dir_ls(path = nc_dir, glob = "*.nc")
+path_file(nc_files)
+
+# como o nome dos arquivos especifica o nome dos modelos
+model_nm <- "CanCM4i"
+model_files <- nc_files[grep(model_nm, nc_files)]
+
+# variaveis e dimensoes com funcao do pacote metR
+nc_info <- GlanceNetCDF(model_files[1])
+nc_info
+# nc_info é uma lista
+#str(info)
+# lead time pode ser obtido dela
+str(nc_info$dims$L)
+(lt <- nc_info$dims$L$vals)
+
+
+# Processamento dos dados de um modelo, para cada lead time,
+# agregando os dados de todos os anos.
+rds_files_by_lt <- lapply(lt,
+                          function(i_lt){
+                            cat(i_lt, "\n")
+                            data_model_lt(
+                              lead_time = i_lt,
+                              nc_model_files = model_files,
+                              var_name = "prec"
+                            )
+                          }
+) 
+
+rds_files_by_lt <- unlist(rds_files_by_lt)
+rds_files_by_lt
+
+
+
+
 
 #library(terra)
 #library(tidyverse)
@@ -25,55 +104,7 @@ here()
 
 
 
-#--------------------------------------------------------------------------
-## listar todos os arquivos das previsões retrospectivas da precipitação CanCM4i
-nc_dir <- here("output", "prec")
-nc_files <- fs::dir_ls(path = nc_dir, glob = "*.nc")
 
-# verificação do numero de arquivos com periodos
-model_counts <- tibble(file = nc_files, 
-       modelo = model_name(nc_files, vname = "prec"),
-       ano = fs::path_file(nc_files) %>%
-         str_extract_all("[0-9]{4}") %>%
-         unlist() %>%
-         as.integer()       
-       ) %>%
-  group_by(modelo) %>%
-  summarise(start = min(ano), end = max(ano), freq = n()) %>%
-  mutate(check_span = end-start+1)
-model_counts
-saveRDS(model_counts, file = here("output", "rds", "model_counts.RDS"))
-# modelo       start   end  freq check_span
-# <chr>        <int> <int> <int>      <dbl>
-# 1 CanCM4i       1981  2018    38         38
-# 2 CanSIPSv2     1981  2018    38         38
-# 3 CMC1-CanCM3   1981  2010    30         30
-# 4 CMC2-CanCM4   1981  2010    30         30
-# 5 GEM-NEMO      1981  2018    38         38
-# 6 NASA-GEOSS2S  1981  2017    37         37
-# 7 NCAR-CESM1    1980  2010    31         31
-# 8 NCEP-CFSv2    1982  2010    29         29
-
-
-# como o nome dos arquivos especifica o nome dos modelos
-#model_nm <- "CanCM4i"
-(model_nms <- model_counts$modelo)
-#files_model <- nc_files[grep(model_nms, nc_files)]
-
-# looping para processar dados (excluindo os modelos já processados)
-map(model_nms[-c(1:4)],
-    function(imodel){
-       # imodel = model_nms[3]
-      cat(imodel, "-------\n", "\n")
-      gc()
-      proc_ncs_by_lt(model = imodel, ncfiles_d = nc_dir)
-    })
-
-
-# 
-# 
-# 
-# 
 # 
 # # Agora faça o looping para obter os dados de todos
 # 

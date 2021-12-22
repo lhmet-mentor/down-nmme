54a196afd10b07524bff7d994a1607dcc475f2da
lhmet <lhmet@ufsm.br>
2021-12-22 18:59:59 -0300

    include code with the processing of all files from a model

--- R/dados-brutos.R
+++ R/dados-brutos.R
@@ -1,72 +1,62 @@
 pcks <- c("terra", "tidyverse", "here", "checkmate", "metR", "fs", "glue")
 easypackages::libraries(pcks)
 
-##### BASE DE DADOS - PREVISÃO RESTROSPECTIVA NNME - CanCM4i #####
-
-# se você trabalha com Projeto do RStudio ele já abre diretamente
-# no dir do projeto (clicando 2x no .Rproj).
-# Evite usar setwd() ao trabalhar com projetos.
-# setwd('~/github/download-hindcast-NMME/output/prec/CanCM4i/')
-# trabalhe especificando caminhos a partir do caminho do projeto com o pacote
-# here
-
-here()
-# "~/Dropbox/github/my_reps/lhmet/download-hindcast-NMME"
-
+## Funções para processamento dos dados netcdf------------------------------
+source(here("R", "data-proc-nc.R"))
 
 ## util somente no caso de obter os dados do google drive ----------------------
 # source(here("R", "unzip-nc.R"))
 # unzip_ncs()
 
-## Funções para processamento dos dados netcdf----------------------------------
-source(here("R", "data-proc-nc.R"))
-
-#-------------------------------------------------------------------------------
-nc_dir <- here("output", "prec")
-count_ncs <- fs::dir_ls(nc_dir) %>%
-  fs::path_ext() %>%
-  table()
-# total de arquivos nc
-count_ncs
-# 271
-
 ## listar todos os arquivos das previsões retrospectivas da precipitação CanCM4i
+nc_dir <- here("output", "prec")
+
+# count_ncs <- fs::dir_ls(nc_dir) %>%
+#   fs::path_ext() %>%
+#   table()
+## total de arquivos nc
+# count_ncs
+#  271
+
 nc_files <- fs::dir_ls(path = nc_dir, glob = "*.nc")
-path_file(nc_files)
+
+model_counts <- nc_files_by_model_year(nc_files)
+# modelo       start   end  freq check_span
+# <chr>        <int> <int> <int>      <dbl>
+# 1 CanCM4i       1981  2018    38         38
+# 2 CanSIPSv2     1981  2018    38         38
+# 3 CMC1-CanCM3   1981  2010    30         30
+# 4 CMC2-CanCM4   1981  2010    30         30
+# 5 GEM-NEMO      1981  2018    38         38
+# 6 NASA-GEOSS2S  1981  2017    37         37
+# 7 NCAR-CESM1    1980  2010    31         31
+# 8 NCEP-CFSv2    1982  2010    29         29
+
 
 # como o nome dos arquivos especifica o nome dos modelos
-model_nm <- "CanCM4i"
-model_files <- nc_files[grep(model_nm, nc_files)]
-
-# variaveis e dimensoes com funcao do pacote metR
-nc_info <- GlanceNetCDF(model_files[1])
-nc_info
-# nc_info é uma lista
-#str(info)
-# lead time pode ser obtido dela
-str(nc_info$dims$L)
-(lt <- nc_info$dims$L$vals)
+#model_nm <- "CanCM4i"
+(model_nms <- model_counts$modelo)
+#files_model <- nc_files[grep(model_nms, nc_files)]
 
 
-# Processamento dos dados de um modelo, para cada lead time,
-# agregando os dados de todos os anos.
-rds_files_by_lt <- lapply(lt,
-                      function(i_lt){
-                        cat(i_lt, "\n")
-                        data_model_lt(
-                          lead_time = i_lt,
-                          nc_model_files = model_files,
-                          var_name = "prec"
-                        )
-                      }
-) 
-
-rds_files_by_lt <- unlist(rds_files_by_lt)
-rds_files_by_lt
+# # para obter informacao do leat time
+# model_nm <- "CanCM4i"
+# model_files <- nc_files[grep(model_nm, nc_files)]
+# # variaveis e dimensoes com funcao do pacote metR
+# nc_info <- GlanceNetCDF(model_files[1])
+# nc_info
+# # nc_info é uma lista
+# #str(info)
+# # lead time pode ser obtido dela
+# str(nc_info$dims$L)
+# (lt <- nc_info$dims$L$vals)
 
 
-# Próximo passo aplicar aos demais modelos 
-# looping em 'model_nm' 
-# escrever função que a partir de model_nm retorne lista de arquivos rds
-# como a acima
-
+# looping para processar dados (excluindo os modelos já processados)
+map(model_nms,
+    function(imodel){
+      # imodel = model_nms[1]
+      cat(imodel, "-------\n", "\n")
+      gc()
+      proc_ncs_by_lt(model = imodel, ncfiles_d = nc_dir)
+    })

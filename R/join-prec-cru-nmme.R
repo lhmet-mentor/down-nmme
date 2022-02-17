f545405eb29f8b2aa9c1b6784baa03bf84d140e1
lhmet <lhmet@ufsm.br>
2022-02-17 19:22:10 -0300

    check dispersion of members

--- R/join-prec-cru-nmme.R
+++ R/join-prec-cru-nmme.R
@@ -2,19 +2,43 @@
 pcks <- c(
   "tidyverse", "here",
   "checkmate", "lubridate",
-  "tictoc"
+  "tictoc", "openair"
 )
 easypackages::libraries(pcks)
 
 source(here("R/data-proc-rds.R"))
 source(here("R/tidy-basin-data.R"))
+source(here("R/utils.R"))
+
 
 
 nmme_cru_basin_data <- join_cru_nmme_basin_data(
   sp_average = "weighted",
   ext = "qs",
   var_name = "prec",
-  stat = "identity",
-  out_dir = here(glue::glue("output/{ext}/basin-avgs/{sp_average}"))
+  stat = "identity"
 )
 
+(models_summary <- import_bin_file("output/qs/model_counts.qs"))
+top6()
+
+
+
+## ensemble averages
+# d <- nmme_cru_basin_data[["data"]][[8]] %>%
+#   #filter(year(Sr) %in% 2001) %>% 
+#   filter(codONS == 24) %>%
+#   mutate(L = factor(trunc(L))) %>%
+#   pivot_wider(names_from = "member", 
+#               values_from = "prec_model", 
+#               names_glue = "{.value}_{member}"
+#               ) %>%
+#   pivot_longer(contains("prec"), names_to = "source", values_to = "value") %>%
+#   mutate(source = as.factor(source))
+# 
+# d %>%
+#   ggplot(aes(x = date, y = value, colour = source)) +
+#   geom_line() +
+#   facet_grid(vars(L), vars(month(date)))
+  
+  

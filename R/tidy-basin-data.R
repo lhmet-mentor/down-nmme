a62174365eed895139158b101bf5dda64b005e1f
lhmet <lhmet@ufsm.br>
2022-02-17 16:53:53 -0300

    tidy data functions before join obs and nmme models data

---
+++ R/tidy-basin-data.R
@@ -0,0 +1,91 @@
+
+#' Tidy basin average data from models in NMME system
+#'
+#' @param var_name e.g. 'prec', 'tmax'
+#' @param out_dir e.g `output/{qs,RDS}/basin-avgs/{arithmetic, weighted}`
+#' @param sp_average 'arithmetic' or 'weighted'
+#' @param ext file extension for read/write processed data, 'qs' or 'RDS' are accepted
+#'
+#' @return tidy data in a tibble
+#' @export
+#'
+#' @examples
+tidy_nmme_basin_data <- function(var_name = "prec", 
+                                 out_dir = here(glue::glue("output/{ext}/basin-avgs/{sp_average}")),
+                                 sp_average = "weighted",
+                                 ext = "qs") {
+  
+  basin_avg_nmme_file <- here(
+    out_dir,
+    glue::glue("nmme-models-{sp_average}-avg-basins-ons.{ext}")
+  )
+  
+  checkmate::assert_file_exists(basin_avg_nmme_file)
+  
+  # resumo de informacoes dos modelos
+  path_model_summary <- fs::path_split(out_dir) %>% 
+    `[[`(., 1) 
+  path_model_summary <- fs::path_join(
+    path_model_summary[1:(grep("basin", path_model_summary)-1)]
+  )
+  models_summary <- import_bin_file(glue::glue("{path_model_summary}/model_counts.{ext}"))
+  
+  # importa dados nmme medias nas bacias
+  prec_nmme_avg_basin <- import_bin_file(basin_avg_nmme_file) %>%
+    mutate(data = map(
+      data, ~ .x %>%
+        rename("date" = date_lead)
+    ))
+  
+  # prec_nmme_avg_basin[["data"]][[1]] # wide
+  # adiciona info dos membros, leads e freq (#anos de previsoes retorospectivas)
+  prec_nmme_avg_basin <- prec_nmme_avg_basin %>%
+    inner_join(dplyr::select(models_summary, modelo, M, L, freq),
+               by = c("model" = "modelo")
+    ) %>%
+    dplyr::rename("n_members" = "M")
+  
+  # tidy nmme data considering 'stat'
+  if (stat == "identity") {
+    # pivota membros nas linhas
+    tictoc::tic()
+    prec_nmme_avg_basin_long <- prec_nmme_avg_basin %>%
+      dplyr::mutate(data = purrr::map(
+        data,
+        ~ .x %>%
+          tidyr::pivot_longer(
+            cols = dplyr::contains(var_name),
+            names_to = "member",
+            names_prefix = paste0(glue::glue("{var_name}_")),
+            names_transform = list(member = as.integer),
+            values_to = paste0(glue::glue("{var_name}_model"))
+          ) %>%
+          dplyr::mutate(prec_model = prec_model * 30) %>% # 30 eh o num de dias no calendario dos modelos
+          # because pivot_longer expand members to the max 
+          # members of the models (24)
+          dplyr::filter(!is.na(prec_model)) #%>%
+        #dplyr::arrange(S, L, codONS, member)
+      ))
+    
+    tictoc::toc()
+    # 124.885 s
+    
+    gc()
+  } else {
+    prec_nmme_avg_basin_long <- prec_nmme_avg_basin %>%
+      dplyr::mutate(data = purrr::map(
+        data,
+        ~ .x %>%
+          dplyr::rename("prec_model" = prec) %>%
+          dplyr::mutate(
+            prec_model = prec_model * 30,
+            member = 0L
+          ) #%>% # 30 eh o num de dias no calendario dos modelos
+        #dplyr::arrange(S, L, codONS, member)
+      ))
+  }
+  
+  
+  prec_nmme_avg_basin_long
+  
+}

cdf6b72979242ea753403411a60dbd77fdf6af62
lhmet <lhmet@ufsm.br>
2022-05-31 17:31:30 -0300

    para remover calculo da previsão climatologica pois agora ela já vem com os dados

--- plot-nmme-members-cru-prec.R
+++ plot-nmme-members-cru-prec.R
@@ -17,56 +17,102 @@ source("R/data-proc-rds.R")
 # principais bacias do SIN
 top6()
 
+.members_names <- function(data_long, col = "previsao"){
+  up <- unique(data_long[[col]])
+  up <- up[grep("member", up)]
+  up
+}
+
+
+
 #------------------------------------------------------------------------------
 # previsoes membros, modelos e media ensemble
 data_nmme_cru_file <- here("output/qs/basin-avgs/weighted/nmme-cru-mly-weighted-avg-basins-ons-ens-members-models-ens-mean-prec-1982-2010.qs")
 data_nmme_cru <- import_bin_file(data_nmme_cru_file)
 
 
+plot_members_nmme <- function(
+         model = "CanSIPS-IC3", 
+         cod_ons = 6, 
+         months = c(1, 4, 8),
+         leads = 0:2,
+         highlights = c("climatology", "obs_avg", "ens_avg", "model_avg")
+){
 
-imodel <- "CanSIPS-IC3"
-ibasin <- 6
-months_plot <- c(1, 7, 10)
-leads_plot <- 0:2
+  titulo <- paste0("Modelo: ", imodel)
+  subtitulo <- paste0("Bacia Hidrográfica: ", stn_name(code = ibasin))
+  
+  data_plot <- data_nmme_cru %>%
+    dplyr::select(model:date, 
+                  model_avg, obs_avg, ens_avg, 
+                  contains("member"),
+                  -Sr
+    ) %>%
+    dplyr::mutate(month = as.integer(lubridate::month(date)),
+                  month = ordered(month, levels = sort(unique(month)))
+                  
+    ) %>%
+    dplyr::relocate(month, .after = "L") %>%
+    dplyr::filter(month %in% months_plot, 
+                  L %in% leads_plot, 
+                  codONS == ibasin,
+                  model == imodel
+    ) %>%
+    tidyr::pivot_longer(cols = -c(model:date), 
+                        names_to = "previsao", 
+                        values_to = "prec"
+    ) %>%
+    tidyr::drop_na(prec)
+  
+  memb_nms <- .members_names(data_plot, "previsao")
+  
+  # juntar com a climatologia
+  
+  
+  data_plot <- dplyr::mutate(data_plot,
+                             previsao = ordered(previsao, levels = c(memb_nms, highlights)
+                             )
+  )
+  
+  
+}
 
-titulo <- paste0("Modelo: ", imodel)
-subtitulo <- paste0("Bacia Hidrográfica: ", stn_name(code = ibasin))
 
-data_plot <- data_nmme_cru %>%
-  dplyr::select(model:date, 
-                model_avg, obs_avg, ens_avg, 
-                contains("member"),
-                -Sr
-                ) %>%
-  dplyr::mutate(month = as.integer(lubridate::month(date)),
-                month = ordered(month, levels = sort(unique(month)))
-  ) %>%
-  dplyr::relocate(month, .after = "L") %>%
-  dplyr::filter(month %in% months_plot, 
-                L %in% leads_plot, 
-                codONS == ibasin,
-                model == imodel
-                ) %>%
-  tidyr::pivot_longer(cols = -c(model:date), 
-                      names_to = "previsao", 
-                      values_to = "prec"
-                      ) %>%
-  dplyr::filter(!is.na(prec))
+
+#library(Polychrome)
+## build-in color palette
+#Glasbey <- glasbey.colors(32)
+
+
+
+#levels(data_plot$previsao)
+
+# # ckeck model_avg values
+# mod_avgs <- data_plot %>%
+#   filter(previsao %in% paste0("member_", 1:20)) %>%
+#   group_by(model, codONS, L, date) %>%
+#   summarise(prec_avg = mean(prec), .groups = "drop") %>%
+#   arrange(L, date)
+# 
+# data_nmme_cru %>% 
+#   filter(codONS == ibasin, model == imodel,
+#          month(date) %in% months_plot, L %in% leads_plot) %>%
+#   group_by(model, codONS, L, date) %>%
+#   arrange(L)
+#! OK
 
 #data_plot %>% filter(L == 1, month == 1, previsao == "member_1")
 
-data_cli <- dplyr::filter(data_plot, previsao == "obs_avg") %>%
-  dplyr::group_by(L, month) %>%
-  dplyr::summarise(climatology = mean(prec), .groups = "drop") 
 
-vars_exc <- data_plot %>%
-  dplyr::filter(str_detect(previsao, "avg")) %>%
-  pull(previsao) %>%
-  unique()
+
+# subconjunto para linhas a destacar
+vars_exc <- tail(levels(data_plot$previsao), 3)
 
 p <- data_plot %>%
-  dplyr::filter(!previsao %in% vars_exc) %>%
-  ggplot(aes(x = date, y = prec, colour = previsao)) +
+  #dplyr::filter(!previsao %in% vars_exc) %>%
+  ggplot(aes(x = date, y = prec, 
+             colour = previsao, linetype = previsao, size = previsao,
+             alpha = previsao)) +
   geom_line() +
   facet_grid2(vars(L), vars(month),
               scales = "free", independent = "y"
@@ -84,33 +130,53 @@ p <- data_plot %>%
     legend.position = "top",
     legend.direction = "horizontal"
   ) +
-  geom_line(
-    data = filter(data_plot, previsao %in% vars_exc),
-    aes(x = date, y = prec, colour = previsao), size = 1
+  scale_colour_manual(
+    values = c(colors_distintc, c("black", "red", "blue")),
+    labels = levels(data_plot$previsao)
   )
+  #scale_colour_metro_d() 
+  
+  # geom_line(
+  #   data = filter(data_plot, previsao %in% tail(levels(data_plot$previsao), 3)),
+  #   aes(x = date, y = prec, colour = previsao), size = 1
+  # )
 
+# pega cores usadas no plot
 g <- ggplot_build(p)
 cols <- unique(g$data[[1]]["colour"])
-labs <- data_plot %>%
-  dplyr::filter(!previsao %in% vars_exc) %>%
-  dplyr::pull(previsao) %>%
-  unique() %>%
-  as.character()
-
-
 
 p <- p +
-  scale_colour_manual(
-    values = c(cols[[1]], c("red", "black", "blue")),
-    labels = c(labs, vars_exc)
+  # scale_colour_manual(
+  #   values = c(colors_distintc, c("black", "red", "blue")),
+  #   labels = levels(data_plot$previsao)
+  # ) +
+  # scale_alpha_manual(
+  #   values = c(rep(0.2, length(labs)), rep(1, length(vars_exc)))
+  # ) +
+  scale_size_manual(values = c(rep(0.5, length(head(cols[[1]], -3))),
+                               c(1.2, 1.6, 1.4)
+                               ),
+                    labels = levels(data_plot$previsao)
+                    ) + 
+  scale_linetype_manual(values = c(rep(1, length(head(cols[[1]], -3))),
+                                   c(1, 3, 4)),
+                        labels = levels(data_plot$previsao)
+  ) +
+  scale_alpha_manual(values = c(rep(0.4, length(head(cols[[1]], -3))),
+                                rep(1, 3)),
+                     labels = levels(data_plot$previsao)
   ) +
   geom_hline(
     data = data_cli,
     aes(yintercept = climatology), colour = "black", lty = 2
   ) +
-  ggtitle(titulo, subtitulo)
+  ggtitle(titulo, subtitulo) 
+  
 p
 
-
-# model_avg errada!
+# p +
+#  geom_line(
+#    data = filter(data_plot, previsao %in% tail(levels(data_plot$previsao), 3)),
+#    aes(x = date, y = prec, linetype = previsao), size = 1
+#  )
 

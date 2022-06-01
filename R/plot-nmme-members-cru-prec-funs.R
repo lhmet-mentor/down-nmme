9d29505bc73f1fe23cf7436f4adef2e01569fa97
lhmet <lhmet@ufsm.br>
2022-06-01 19:41:13 -0300

    adaptacao da paleta glasbey para conj de previsoes (max = 28)

--- R/plot-nmme-members-cru-prec-funs.R
+++ R/plot-nmme-members-cru-prec-funs.R
@@ -6,6 +6,57 @@ source(here("R/data-proc-rds.R"))
 # principais bacias do SIN
 #top6()
 
+
+
+# distinctive colours
+# https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+# https://stackoverflow.com/questions/9563711/r-color-palettes-for-many-data-classes/41230685#41230685
+colors_distintct <- function(n){
+  # colors_distintc <- c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', 
+  #                      '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', 
+  #                      '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', 
+  #                      '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080'
+  # )
+  #n = 28
+  black_blue_red <- c("#000033", "#0000FF", "#FF0000")
+  pg <- pals::glasbey(n)
+  #pie(rep(1, length(pg)), col = pg)
+  pg <- c(pg[!(pg %in% black_blue_red)], black_blue_red[c(1,1,2:3)])
+  pg[pg == "#201A01"] <- "#BEBEBE"
+  #pg[c(26:27)] <- "#000000"
+  #pie(rep(1, length(pg)), col = pg)
+  pg
+  #pie(rep(1, length(pg)), col = pg)
+}
+
+
+
+# library(Polychrome)
+## build-in color palette
+# n = 24
+# pie(rep(1, n), col = Polychrome::palette36.colors(n = 24))
+# BOA OPCAO
+# pie(rep(1, n), col = Polychrome::glasbey.colors(n = 24))
+
+# library(randomcoloR)
+# n <- 24
+# palette <- distinctColorPalette(n)
+# pie(rep(1, n), col=palette)
+
+
+# library(RColorBrewer)
+# distinct_colors <- function(n = 28) {
+#   qual_col_pals <- brewer.pal.info[brewer.pal.info$category == "qual", ]
+#   col_vector <- unlist(
+#     mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals))
+#   )
+#   cores <- sample(col_vector, n)
+#   pie(rep(1, n), col = cores)
+# }
+
+
+#-------------------------------------------------------------------------------
+# Obtem nomes das variaveis com os membros e ordena
 .members_names <- function(data_long, col = "previsao") {
   up <- unique(data_long[[col]])
   up <- up[grep("member", up)]
@@ -27,9 +78,10 @@ plot_members_nmme <- function(
                               months = c(1, 4, 8),
                               leads = 0:2,
                               highlights = c("climatology", "obs_avg", "ens_avg", "model_avg"),
-                              alpha_memb = 0.5,
+                              alpha_memb = 0.4,
                               ncols_legend = 8) {
 
+  #checkmate::assert_
   titulo <- paste0("Modelo: ", imodel)
   subtitulo <- paste0("Bacia Hidrográfica: ", stn_name(code = cod_ons))
 
@@ -65,6 +117,8 @@ plot_members_nmme <- function(
     )
   )
 
+  n_colors <- length(c(memb_nms, highlights))
+  
   p <- data_plot %>%
     # dplyr::filter(!previsao %in% vars_exc) %>%
     ggplot(aes(
@@ -91,15 +145,10 @@ plot_members_nmme <- function(
       legend.position = "top",
       legend.direction = "horizontal"
     ) +
-    scale_colour_manual(
-       values = c(colors_distintc, c("black", "black", "red", "blue")),
-       # values = c(
-       #   Polychrome::glasbey.colors(n = n_memb),
-       #   c("#000000", "#000000", "#FF0000", "#0000FF")
-       # ),
-      labels = levels(data_plot$previsao)
-    )
-  p <- p +
+    # scale_colour_manual(
+    #    values = c(colors_distintct(), c("black", "black", "red", "blue")),
+    #   labels = levels(data_plot$previsao)
+    # )
     scale_size_manual(
       values = c(
         rep(0.5, n_memb),
@@ -119,6 +168,10 @@ plot_members_nmme <- function(
         rep(alpha_memb, n_memb),
         rep(1, length(highlights))
       ),
+      labels = levels(data_plot$previsao)
+    ) +
+    scale_colour_manual(
+      values = colors_distintct(n_colors-1),
       labels = levels(data_plot$previsao)
     ) +
     ggtitle(titulo, subtitulo) +

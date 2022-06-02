05ed46c9cf0109c50ffda34443f2f23b8913ad51
lhmet <lhmet@ufsm.br>
2022-06-02 19:14:44 -0300

    movendo parte de análise para select-features

--- R/teste-bc.R
+++ R/teste-bc.R
@@ -8,7 +8,8 @@ pcks <- c(
   "downscaleR",
   "tidymodels",
   "crch",
-  "MBC" # 
+  "MBC",
+  "DataExplorer" 
   )
 
 easypackages::libraries(pcks)
@@ -28,139 +29,52 @@ source("R/bc-funs.R")
 top6()
 
 # importa dados combinados das medias dos modelos e da media ensemble
-data_join_file <- here("output/qs/basin-avgs/weighted",
-                       "nmme-cru-mly-weighted-avg-basins-ons-ens-members-ens-mean-prec-1982-2010.qs")
+data_nmme_cru_file <- here("output/qs/basin-avgs/weighted/",
+                           "nmme-cru-mly-weighted-avg-basins-ons-ens-members-models-ens-mean-prec-1982-2010.qs")
+data_nmme_cru <- import_bin_file(data_nmme_cru_file)
 
-# importa dados combinados (ens e medias modelos)
-data_pp <- import_bin_file(data_join_file) 
-unique(data_pp$model)
 
 # data_pp %>%
 #   group_by(model) %>%
 #   tally()
 
-# verificacao das distribuicoes de prec 
-# (aproximacao gaussiana para chuva mensal)
-#bc0p2 <- function(x) (x + (0.01 * mean(x, na.rm = TRUE))^0.2)/0.2
-
-data_pp %>%
-  filter(codONS %in% top6()$codONS, month(date) == 8) %>%
-  #mutate()
-  #ggplot(aes(x = bc0p2(model_avg))) +
-  ggplot(aes(x = model_avg)) +
-  #ggplot(aes(x = obs_avg)) +
-  geom_histogram(bins = 20) +
-  facet_grid(vars(codONS), vars(model))
-
-
-  
-
-# dados com os modelos nas colunas para usar com metodos multimodelos
-# data_pp_wide <-  data_pp %>%
-#   select(-())
-#   mutate(model = str_replace_all(model, "-", "_")) %>%
-#   select(-Sr) %>%
-#   pivot_wider(
-#     names_from = "model", 
-#     values_from = "model_avg"
-#     #names_prefix = ""
-#   ) %>%
-#   tail()
-
-
-  
-
-
-
-
 
 # Dados para teste de aplicacao BC---------------------------------------------
- imodel <- "CanSIPS-IC3"
- ibasin <- 6
- month <- 1
+ imodel = "CanSIPS-IC3"
+ ibasin = 6
+ month = 1
+ lead_time = 1
 
-data_pp_1model <- data_pp %>%
-  #select(-model_sd) %>%
-  #dplyr::filter(codONS %in% top6()[[1]], L == 1, month(date) == month) %>%
-  dplyr::filter(codONS == ibasin, month(date) == month, model == imodel, L == 1) 
-  
-
-d <- data_pp %>%
-  #select(-model_sd) %>%
-  #dplyr::filter(codONS %in% top6()[[1]], L == 1, month(date) == month) %>%
-  dplyr::filter(codONS == ibasin, month(date) == month, L == 1)
-d %>%
-  ggplot(aes(x = date, y = model_avg, color = model)) + 
-  geom_line() +
-  geom_line(data = distinct(d, date, obs_avg),
-            aes(x = date, y = obs_avg), color = 1)
-
-
-rng <- range(select(data_pp_1model, model_avg, obs_avg, ens_avg))
-rng <- c(trunc(rng[1]), ceiling(rng[2]))
-
-data_pp_1model_long <- data_pp_1model %>%
-  select(-c(ens_sd, model_sd)) %>%
-  pivot_longer(cols = -c(model:date), names_to = "prec", values_to = "value")
-  
-data_pp_1model_long %>%
-  ggplot(aes(x = date, y = value, color = prec)) +
-  geom_point() + 
-  geom_line() +
-  theme_bw() +
-  geom_hline(yintercept = mean(data_pp_1model$obs_avg))
-
-openair::scatterPlot(data_pp_1model, 
-                     x = "obs_avg", 
-                     y = "ens_avg", 
-                     linear = TRUE, 
-                     mod.line = TRUE,
-                     #type = c("model", "codONS")
-                     pch = 20, 
-                     xlim = c(rng), 
-                     ylim = rng
-                     
-)
-openair::scatterPlot(data_pp_1model, 
-                     x = "obs_avg", 
-                     y = "model_avg", 
-                     linear = TRUE, 
-                     mod.line = TRUE,
-                     #type = c("model", "codONS")
-                     pch = 20, 
-                     xlim = c(rng), 
-                     ylim = rng
-                     
-)
-
-data_pp_1model_long %>%
-  ggplot(aes(x = value, color = prec)) +
-  geom_density()
-
-
-
+data_pp_1model <- data_nmme_cru %>%
+  dplyr::filter(codONS == ibasin, 
+                month(date) == month, 
+                model == imodel, 
+                L == lead_time
+                ) 
 
 #-------------------------------------------------------------------------------
 # separacao dos dados em periodos de treino (calibracao) e teste (validacao)
 # IDEAL: "treino" seria o periodo das hindcasts (1982-2010)
 #        "teste"  seria o periodo das forecasts (previsões) de 2011-2020 
 
-
-
 ## separacao dos dados aleatoria
 # dados_pp_split <- initial_split(dados_pp, prop = 0.6) 
 ## prop: proporcao das obs que serao usadas para treino
 ## 1 - prop: proporcao das obs usada para teste 
 
-
 ## como nao temos os dados baixados do periodo de previsao usaremos outra forma
 ## alternativa: validacao cruzada
+unique(month(data_pp_1model$date))
+unique(data_pp_1model$L)
 
-dados_pp_split <- loo_cv(data_pp_1model)
+data_pp <- data_pp_1model %>%
+  filter(month(date) == 1)
+
+dados_pp_split <- loo_cv(data_pp)
 
 
 dados_pp_cv <- map_df(
-  1:nrow(dados_pp_split),
+  1:nrow(data_pp),
   function(ires) {
     # ires = 9
     # primeira reamostra
@@ -197,6 +111,14 @@ dados_pp_cv <- map_df(
         enssd = treino$ens_sd,
         ensmean_new = ens_avg,
         enssd_new = ens_sd
+      ),
+      vi = variance_inflation(
+        obs = treino$obs_avg,
+        prev = treino$model_avg,
+        ens = treino$ens_avg,
+        ens_sd = treino$ens_sd,
+        prev_new = model_avg,
+        ens_new = ens_avg
       )
       # inserir outros metodos aqui
       # ...
@@ -208,14 +130,12 @@ dados_pp_cv <- map_df(
 
 dados_pp_cv <- arrange(dados_pp_cv, model, codONS, date)
 
-
-
 dados_pp_cv_long <- dados_pp_cv %>%
-  select(-ens_sd) %>%
+  select(-ens_sd, -model_sd, -contains("member"), -month) %>%
   pivot_longer(-(model:date), names_to = "prec", values_to = "value")
 
 dados_pp_cv_long %>%
-  filter(prec %in% c("obs_avg", "mos", "ens_avg", "ngr2")) %>%
+  filter(prec %in% c("obs_avg", "mos", "ens_avg", "vi")) %>%
   ggplot(aes(x = date, y = value, color = prec)) +
   #geom_col(position = "dodge") + 
   geom_point() +
@@ -224,6 +144,7 @@ dados_pp_cv_long %>%
 
 
 dados_pp_cv4plot <-  dados_pp_cv %>%
+  janitor::remove_empty(which = "cols") %>%
   select(-ens_sd) %>%
   pivot_longer(cols = -c(model:date, obs_avg),
                names_to = "models",

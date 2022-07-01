9a0123a0ddbee5d12e1ede2ead2129c42b6e4653
GuilhermeTorma <guilherme_torma99@hotmail.com>
2022-07-01 11:51:05 -0300

    -Função pegar o nome da variável de cada modelo (link) -Ifelse para pegar o tipo do modelo (link) -Alterando de pt para en´

--- R/down-nmme.R
+++ R/down-nmme.R
@@ -3,6 +3,13 @@
 
 download_file_safe <- purrr::safely(download.file)
 
+.pick_varname <- function(.model, .variable){
+  var_name_down <- names_vars_models() %>%
+    dplyr::filter(model == .model) %>%
+    dplyr::select(all_of(.variable)) %>%
+    dplyr::pull()  
+  var_name_down
+}
 
 
 #' Baixa o arquivo anual da previsao retrospectiva do NMME para a AS
@@ -18,52 +25,56 @@ download_file_safe <- purrr::safely(download.file)
 #' @export
 #'
 #' @examples
-down_nmme <- function(ano = 1981, 
-                      modelo = "CanCM4i", 
-                      variavel = "prec", 
-                      tipo = "hindcast"){
-  tipo <- toupper(tipo)
-  # ano = 1980;  modelo = "NASA-GEOSS2S"; variavel = "prec"
-  # ano = 1992;  modelo = "GFDL-SPEAR"; variavel = "prec"
+down_nmme_by_ymv <- function(year = "1980", 
+                             model = "CanCM4i", 
+                             variable = c("prec", "tmax", "tmin"),
+                             type = c("HINDCAST", "FORECAST", "MONTHLY"),
+                             overwrite = FALSE
+){
+  type <- toupper(type)
+  # year = 2000;  model = "NASA-GEOSS2S"; variable = "tmax"; type = "HINDCAST"
+  # year = 1990;  model = "CanCM4i"; variable = "tmin"; type = "HINDCAST"
   
-  ano <- as.character(ano)
+  year <- as.character(year)
+  
+  variable_model <- .pick_varname(model, variable) 
+  
+  type_link <- ifelse(type == "MONTHLY", "MONTHLY", paste0("{type}","/.MONTHLY")) %>%
+    glue::glue(.)
   
   data_link <- paste0(
     "http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME/",
     # modelo
-    ".{modelo}/.{tipo}/.MONTHLY/",
+    ".{model}/",
+    #tipo
+    ".{type_link}/",
     # variavel
-    ".{variavel}/",
+    ".{variable_model}/",
     # Forecast Start Time (forecast_reference_time)
-    "S/%280000%201%20Jan%20{ano}%29%280000%2030%20Dec%20{ano}",
+    "S/%280000%201%20Jan%20{year}%29%280000%2030%20Dec%20{year}",
     # sub dominio
     "%29RANGEEDGES/X/%2830W%29%2885W%29RANGEEDGES/Y/%2860S%29%2815N%29RANGEEDGES/",
     # nome dop arq default
     "data.nc"
   )
   
-  prefix <- paste0("nmme_", variavel, "_", modelo, "_", ano) 
+  prefix <- paste0("nmme_",  variable, "_", model, "_", year) 
   file <- stringr::str_replace(fs::path_file(data_link), "data", prefix)
-  dest_file <- here::here("output", variavel, file) 
+  dest_file <- here::here("output",  variable, file) 
   
   Sys.sleep(1)
   
-
-  # data_link_ano <- stringr::str_replace_all(data_link, "variavel", variavel)
-  # data_link_ano <- stringr::str_replace_all(data_link_ano, "modelo", modelo)
-  # data_link_ano <- stringr::str_replace_all(data_link_ano, "YYYY", ano)
+  data_link_year <- glue::glue(data_link)
   
-  data_link_ano <- glue::glue(data_link)
-  
-  #path_file(data_link_ano)
+  #path_file(data_link_year)
   message("Baixando arquivo: ", dest_file)
   
-  res <- download_file_safe(data_link_ano, destfile = dest_file, mode = "wb")
+  res <- download_file_safe(data_link_year, destfile = dest_file, mode = "wb")
   
   
   
   if(!is.null(res$error)){ # arquivo para ano nao encontrado
-    warning("Nao foi possivel baixar o arquivo: \n", data_link_ano)
+    warning("Nao foi possivel baixar o arquivo: \n", data_link_year)
     return(NULL)
   }   
   

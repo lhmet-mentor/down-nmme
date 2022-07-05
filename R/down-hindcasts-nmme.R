2c23d47b11369bc98cba3a1ec08f1078d84bb02b
lhmet <lhmet@ufsm.br>
2022-07-05 15:37:01 -0300

    teste para baixar forecast somente

--- R/down-hindcasts-nmme.R
+++ R/down-hindcasts-nmme.R
@@ -16,15 +16,22 @@ easypackages::libraries(pcks)
 source(here("R/models-nmme.R"))
 #names_vars_models()
 source(here("R/down-nmme.R"))
-
+# para import/export_bin_file()
+source("https://raw.githubusercontent.com/lhmet-mentor/proc-nmme/main/R/data-proc-rds.R")
 
 #------------------------------------------------------------------------------
 # lista de args para down_nmme_by_ymv() na ordem exigida pela funcao
 args_l <- tab_mod_year_vname_type %>%
-  dplyr::filter(type == "FORECAST", vname_ref == "prec")
+  dplyr::filter(type == "FORECAST", vname_ref == "prec") 
 
-baixados_temp <- purrr::pmap(args_l,
+#View(args_l)
+#info_nmme <- import_bin_file(here("output/qs/model_counts.qs"))
+#info_nmme
+
+baixados_temp <- purrr::pmap(as.list(args_l),
                              function(year, model, vname_ref, type) {
                                down_nmme_by_ymv(year, model, vname_ref, type)
                              }
 )
+
+export_bin_file(baixados_temp, here("output/"))
